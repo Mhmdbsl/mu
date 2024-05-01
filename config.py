5b149452309c5afecf5058a56fe1584d0b92220e
Dev-Hamoo <165529701+Dev-Hamoo@users.noreply.github.com>
2024-05-01 11:03:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/sir-ahmed/DEV.git",
+    "https://github.com/Dev-Hamoo/mu.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
